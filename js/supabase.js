bd2d20bb3cb692800120ae47c75290afbf7463b7
supoyomuhammad <beehome1809@gmail.com>
2026-04-06 21:08:09 +0700

    supabase

--- js/supabase.js
+++ js/supabase.js
@@ -1,2 +1,2 @@
-// Menggunakan CDN Supabase versi 2.x sesuai SDD
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Inisialisasi Supabase client dan timpa ke variabel global agar dikenali oleh semua file JS lainnya
+window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
